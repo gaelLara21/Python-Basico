ac01eefbef2e6bf5249b6d7558cf01af91b6a75a
gaelLara21 <gaellara19@gmail.com>
2022-06-26 21:27:01 -0500

    datos sencillos

--- Variables.py
+++ Variables.py
@@ -3,3 +3,36 @@ print(numero)
 
 numero1=5
 numero2=6
+
+numero1 + numero2
+
+numero_resultado= numero1 + numero2
+
+
+year = 2021
+dia = 7
+edad = 26
+temperatura = -5
+angulo = -45
+
+
+pi = 3.1416
+estatura = 1.84
+peso = 85.6
+temperatura = -5.55
+edad = 26.0
+
+
+esta_frio = True
+es_bajo = False
+
+profesor = "Sergio Castaño Giraldo"
+web = 'Control Automático Educación'
+cursos = """
+1. Python
+2. Matlab
+3. MicroPython
+4. Arduino
+5. PIC
+6. Control de Procesos
+"""
